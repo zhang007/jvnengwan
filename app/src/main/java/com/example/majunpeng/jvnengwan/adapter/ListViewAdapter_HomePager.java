3a8ff85d378dc026c2299d5b9b38f66cdb58caab
majunpeng <1548614132@qq.com>
2016-07-26 15:34:00 +0800

    ffsf

---
+++ app/src/main/java/com/example/majunpeng/jvnengwan/adapter/ListViewAdapter_HomePager.java
@@ -0,0 +1,42 @@
+package com.example.majunpeng.jvnengwan.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by majunpeng on 2016/7/26.
+ * ListView多布局的适配器
+ */
+public class ListViewAdapter_HomePager extends BaseAdapter{
+//    private LayoutInflater mInflater;
+//    private List <> data;
+
+    //构造方法
+    public ListViewAdapter_HomePager(){
+//dd
+    }
+
+    //重写的四个方法
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}

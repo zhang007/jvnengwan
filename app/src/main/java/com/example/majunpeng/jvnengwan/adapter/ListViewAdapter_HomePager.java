fb9bb9839ad6660c626523c41101a35ad6073e94
majunpeng <1548614132@qq.com>
2016-07-26 16:05:03 +0800

    ffsf

--- app/src/main/java/com/example/majunpeng/jvnengwan/adapter/ListViewAdapter_HomePager.java
+++ app/src/main/java/com/example/majunpeng/jvnengwan/adapter/ListViewAdapter_HomePager.java
@@ -6,37 +6,71 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.example.majunpeng.jvnengwan.bean.HomePageData;
+
+import java.util.List;
+
 /**
  * Created by majunpeng on 2016/7/26.
  * ListView多布局的适配器
  */
 public class ListViewAdapter_HomePager extends BaseAdapter{
-//    private LayoutInflater mInflater;
-//    private List <> data;
+    private LayoutInflater mInflater;
+
+    //数据源
+    private List <HomePageData> data;
 
     //构造方法
-    public ListViewAdapter_HomePager(){
-//dd
+    public ListViewAdapter_HomePager(Context context,List <HomePageData> data){
+        mInflater=LayoutInflater.from(context);
+        this.data=data;
     }
 
     //重写的四个方法
     @Override
     public int getCount() {
-        return 0;
+        return data.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return data.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
+    //创建正确的ConvertView      ConvertView复用
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+
+
+
+        return convertView;
+    }
+
+    /**
+     * 重写的两个方法===================================
+     * @return
+     */
+
+    //该方法返回多个不同的布局
+    @Override
+    public int getViewTypeCount() {
+        //这个是固定值
+        return super.getViewTypeCount();
+    }
+
+    //该方法根据position   返回相应的Item
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    private  class ViewHolder {
+
     }
 }
---
+++ app/src/main/java/com/example/majunpeng/jvnengwan/bean/HomePageData.java
@@ -0,0 +1,7 @@
+package com.example.majunpeng.jvnengwan.bean;
+
+/**
+ * Created by majunpeng on 2016/7/26.
+ */
+public class HomePageData {
+}

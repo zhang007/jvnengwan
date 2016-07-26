135526cb3e099d720d1156459c1d4c4c510b40cf
ljaaaa <1296533542@qq.com>
2016-07-26 15:06:21 +0800

    增加了OkhttpUtils类  清单文件中添加了网络请求权限

--- app/src/main/java/com/example/majunpeng/jvnengwan/fragment/CategoryFragmen.java
+++ app/src/main/java/com/example/majunpeng/jvnengwan/fragment/CategoryFragmen.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.majunpeng.jvnengwan.R;
+import com.example.majunpeng.jvnengwan.httputils.OkHttpUtils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +25,8 @@ public class CategoryFragmen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        OkHttpUtils okHttpUtils=new OkHttpUtils();
+        okHttpUtils.getString();
         return inflater.inflate(R.layout.fragment_category, container, false);
     }
 

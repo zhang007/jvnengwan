821c196e947d922e513da0ab9fa7f3c2ab481565
ljaaaa <1296533542@qq.com>
2016-07-27 09:26:34 +0800

    第一个页面已经展示

--- app/src/main/java/com/example/majunpeng/jvnengwan/fragment/CategoryFragmen.java
+++ app/src/main/java/com/example/majunpeng/jvnengwan/fragment/CategoryFragmen.java
@@ -7,16 +7,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.majunpeng.jvnengwan.R;
+import com.example.majunpeng.jvnengwan.adapter.L_MyAdapter;
+import com.example.majunpeng.jvnengwan.bean.LmainInfo;
 import com.example.majunpeng.jvnengwan.httputils.OkHttpUtils;
 import com.example.majunpeng.jvnengwan.interface1.OnLStringClick;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CategoryFragmen extends Fragment implements OnLStringClick {
-String josn;
+private String josn;
+private List<LmainInfo.DataListBean>data;
+    private GridView l_gridView;
 
     public CategoryFragmen() {
         // Required empty public constructor
@@ -31,13 +40,26 @@ String josn;
       okHttpUtils.getString("http://zhibo.sogou.com/gamelivedata?sysVer=4.4.2&pageSize=100&appVer=301&type=json&pageNo=1",this);
 
         Log.e("AAA12",""+josn);
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view= inflater.inflate(R.layout.fragment_category, container, false);
+        init(view);
+        data=new ArrayList<>();
+        return view;
     }
-//接口回调
+
+    private void init(View view) {
+        l_gridView = ((GridView) view.findViewById(R.id.l_gridView));
+    }
+
+    //接口回调
     @Override
     public void get(String str) {
     josn=str;
-
-
+        Gson gson=new Gson();
+        LmainInfo lmainInfo=gson.fromJson(josn,LmainInfo.class);
+              data = lmainInfo.getData_list();
+        L_MyAdapter adapter=new L_MyAdapter(getActivity());
+        adapter.addData(data);
+        l_gridView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
